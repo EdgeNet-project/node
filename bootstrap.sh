6bd4d3d22c195774011714c57389680f8466e2d2
Maxime Mouchet <max@maxmouchet.com>
2021-01-21 16:42:01 +0100

    playbook: fix node name, SSH port and k8s version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,64 @@
 #!/bin/sh
 set -eu
 
-# For debugging/development, set this to "file://." or another local directory.
+# URL of the Git repository containing the playbook to run.
 EDGENET_REPOSITORY="${EDGENET_REPOSITORY:-https://github.com/EdgeNet-project/node.git}"
+
+# Name of the playbook to run.
 EDGENET_PLAYBOOK="${EDGENET_PLAYBOOK:-edgenet-node-full.yml}"
 
-# Some systems do not have sudo, in this case do not use it.
+# Name to be used for the edgenet node.
+EDGENET_NODE_NAME="${EDGENET_NODE_NAME:-$(cat /etc/machine-id)}"
+
+# SSH port to be used for remote access.
+EDGENET_SSH_PORT="${EDGENET_SSH_PORT:-25010}"
+
+echo "EDGENET_REPOSITORY=${EDGENET_REPOSITORY}"
+echo "EDGENET_PLAYBOOK=${EDGENET_PLAYBOOK}"
+echo "EDGENET_NODE_NAME=${EDGENET_NODE_NAME}"
+echo "EDGENET_SSH_PORT=${EDGENET_SSH_PORT}"
+
+echo
+echo "To change these values, set the appropriate environement variable."
+echo "For example: 'env EDGENET_SSH_PORT=25010 bootstrap.sh'."
+echo "Press any key to continue, or CTRL+C to exit..."
+read -r _
+
+# Do not use sudo if not installed.
 SUDO=""
 command -v sudo >/dev/null 2>&1 && SUDO="sudo"
+
+# Temporary directory for cloning the repository.
+TMP=$(mktemp -d)
 
 # OS-specific variables.
 ID="Unknown"
 VERSION_ID="Unknown"
 . /etc/os-release
 
+# Install Ansible.
 case "${ID}-${VERSION_ID}" in
     centos-8)
         ${SUDO} yum install --assumeyes epel-release
-        ${SUDO} yum install --assumeyes ansible git python3-openshift
+        ${SUDO} yum install --assumeyes ansible git
         ;;
 
     fedora-32|fedora-33)
-        ${SUDO} dnf install --assumeyes ansible git python3-openshift
+        ${SUDO} dnf install --assumeyes ansible git
         ;;
 
-    debian-9)
-        export DEBIAN_FRONTEND=noninteractive
-        ${SUDO} apt update
-        ${SUDO} apt install --yes dirmngr software-properties-common
-        ${SUDO} apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-        ${SUDO} apt-add-repository --update --yes \
-            "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
-        ${SUDO} apt install --yes ansible git python3-pip
-        ;;
-
-    ubuntu-18*|ubuntu-19*|ubuntu-20*|ubuntu-21*)
+    ubuntu-18*|ubuntu-19*)
         export DEBIAN_FRONTEND=noninteractive
         ${SUDO} apt update
         ${SUDO} apt install --yes dirmngr software-properties-common
         ${SUDO} apt-add-repository --yes --update ppa:ansible/ansible
-        ${SUDO} apt install --yes ansible git python3-pip
+        ${SUDO} apt install --yes ansible git
+        ;;
+
+    ubuntu-20*|ubuntu-21*)
+        export DEBIAN_FRONTEND=noninteractive
+        ${SUDO} apt update
+        ${SUDO} apt install --yes ansible git
         ;;
 
     *)
@@ -49,7 +68,6 @@ case "${ID}-${VERSION_ID}" in
 esac
 
 # Fetch the repository.
-TMP=$(mktemp -d)
 git clone --depth 1 "${EDGENET_REPOSITORY}" "${TMP}"
 
 # Install collections and roles from Ansible Galaxy.
@@ -59,5 +77,7 @@ ansible-galaxy role install --ignore-errors --role-file "${TMP}/requirements.yml
 # Run the node playbook.
 ansible-playbook --connection local \
                  --extra-vars "ansible_python_interpreter=/usr/bin/python3" \
+                 --extra-vars "edgenet_node_name=${EDGENET_NODE_NAME}" \
+                 --extra-vars "edgenet_ssh_port=${EDGENET_SSH_PORT}" \
                  --inventory "localhost," \
                  "${TMP}/${EDGENET_PLAYBOOK}"
