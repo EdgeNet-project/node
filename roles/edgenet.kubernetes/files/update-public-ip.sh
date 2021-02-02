d9d844626dafc6a4453b66b55af229ca4862d205
Maxime Mouchet <max@maxmouchet.com>
2021-02-02 13:40:58 +0100

    edgenet.kubernetes: GCP support

--- roles/edgenet.kubernetes/files/update-public-ip.sh
+++ roles/edgenet.kubernetes/files/update-public-ip.sh
@@ -4,15 +4,24 @@ set -ux
 # Assign public IPs to internal interfaces.
 # Major public clouds rewrite the src/dst IP of outbound/inbound packets.
 
+alias curl="curl --max-time 0.5 --show-error --silent --header 'Metadata-Flavor: Google'"
+
 # EC2
-if curl --max-time 0.5 --show-error --silent http://169.254.169.254/latest/meta-data > /dev/null; then
+if curl http://169.254.169.254/latest/meta-data > /dev/null; then
   echo "Amazon EC2 detected"
-  mac=$(curl --show-error --silent http://169.254.169.254/latest/meta-data/mac)
-  ip4=$(curl --show-error --silent http://169.254.169.254/latest/meta-data/public-ipv4)
+  mac=$(curl http://169.254.169.254/latest/meta-data/mac)
+  ip4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
   dev=$(ip --brief link | grep "${mac}" | cut -d ' ' -f 1)
   ip addr add "${ip4}/32" dev "${dev}" || true
+  exit
 fi
 
 # GCP
-# TODO
-
+if curl http://metadata.google.internal/computeMetadata/v1/instance/ > /dev/null; then
+  echo "Google Compute Engine detected"
+  mac=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/mac)
+  ip4=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+  dev=$(ip --brief link | grep "${mac}" | cut -d ' ' -f 1)
+  ip addr add "${ip4}/32" dev "${dev}" || true
+  exit
+fi
