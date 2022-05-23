94a46acc2bc1a68ceab4a8d316dd497d7971ecb0
Tengfei AN <atf828@example.com>
2022-05-23 22:04:39 +0200

    Bug fix & update instance type to "t3.small",  recommended by EdgeNet

--- bootstrap.aws.local.sh
+++ bootstrap.aws.local.sh
@@ -182,7 +182,7 @@ else
   read -p "Input the number of worker instances for k8s cluster (2 for default, press <Enter> for default):" nb_worker
   nb_worker=${nb_worker:-2}
   # Set instance_type for instances of cluster
-  itype_default="t2.micro"
+  itype_default="t3.small"
   read -p "Input the instance_type for master instances (${itype_default} for default, press <Enter> for default):" itype_master
   itype_master=${itype_master:-${itype_default}}
   read -p "Input the instance_type for worker instances (${itype_default} for default, press <Enter> for default):" itype_worker
